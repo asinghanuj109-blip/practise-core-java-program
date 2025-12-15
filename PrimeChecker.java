5ac9eb3fafc7696670af6fe81055321c78e0e18c
Anuj Singh <asinghanuj109@gmail.com>
2025-12-15 21:14:12 +0530

    Create PrimeChecker.java

---
+++ PrimeChecker.java
@@ -0,0 +1,33 @@
+package year;
+import java.util.Scanner;
+public class PrimeChecker {
+
+	public static boolean primeNumberFinder(int number) {
+        if (number < 2) {
+            return false;
+        }
+
+        for (int i = 2; i * i <= number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+
+        if (primeNumberFinder(num)) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+
+        scanner.close();
+    }
+}
